e123ff6503dd82cd7668ce31528d059e3039a5c1
Rob Meharg <rob.j.meh@gmail.com>
2015-07-28 15:36:41 -0400

    Changed from weird header auth thing to form-data for api/authenticate

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -11,7 +11,7 @@ class ApiAuthController extends Controller
     public function authenticate(Request $request)
     {
         // grab credentials from the request
-        $credentials = [ 'email'=> $request->header('php-auth-user'), 'password'=> $request->header('php-auth-pw') ];
+        $credentials = $request->only('email', 'password');
 
         try {
             // attempt to verify the credentials and create a token for the user
